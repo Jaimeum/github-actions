a052a0edbf940f8d21cc57e85feec04aee7ae590
Jaime Uria <jaime.um19@gmail.com>
2024-05-08 18:17:11 -0600

    error

---
+++ app.py
@@ -0,0 +1,8 @@
+# Python script to calculate the square of a number
+def square(number):
+    # Intentional mistake: variable name 'num' is not defined
+    result = num ** 2
+    return result
+
+# Trying to calculate the square of 5
+print(square(5))
